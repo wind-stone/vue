505da00aa6983488f37723af2e642ba074c67afa
chenxiaolong <chenxiaolongwind@didichuxing.com>
2018-04-11 09:27:47 +0800

    add description

--- src/core/instance/proxy.js
+++ src/core/instance/proxy.js
@@ -1,5 +1,23 @@
 /* not type checking this file because flow doesn't play well with Proxy */
 
+/**
+ * 非生产环境下，会通过原生的`Proxy`对象做一些代理操作，比如
+ * - 给`config.keyCodes`添加代理，禁止通过`Vue.config.keyCodes`覆盖一些内置的修饰符，如
+ *     - `stop`
+ *    - `prevent`
+ *     - `self`
+ *     - `ctrl`
+ *     - `shift`
+ *     - `alt`
+ *     - `meta`
+ *     - `exact`
+ * - 给`vm`添加代理`vm._renderProxy`，以下两种情况取其一：
+ *     - `hasHandler`：判断对象具有某个属性时，如果这个属性不存在且不在允许访问的全局属性/方法列表内，则给出警告
+ *     - `getHandler`：如果使用了对象的某个属性且该属性不存在，给出警告
+
+ * 关于原生的`Proxy`使用，可参考：[阮一峰-ECMAScript 6 入门-Proxy](http://es6.ruanyifeng.com/#docs/proxy)
+ */
+
 import config from 'core/config'
 import { warn, makeMap, isNative } from '../util/index'
 
