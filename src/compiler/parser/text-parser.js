046134e8ed8dbdde3ae4142e96e5003948d51de7
wind-stone <wind-stone@qq.com>
2018-04-29 17:32:45 +0800

    处理插值

--- src/compiler/parser/text-parser.js
+++ src/compiler/parser/text-parser.js
@@ -1,12 +1,46 @@
 /* @flow */
 
+/**
+ * 该文件主要处理文本里插值，返回去除插值之后的表达式
+ */
+
 import { cached } from 'shared/util'
 import { parseFilters } from './filter-parser'
 
 const defaultTagRE = /\{\{((?:.|\n)+?)\}\}/g
+
+/**
+ * 正则表达式分析
+ *
+ * /[
+ *    -.*+?^${}()|[\]\/\\
+ * ]/
+ *
+ * 该正则表达式匹配单个字符，这些字符是：
+ * -
+ * .
+ * *
+ * +
+ * ?
+ * ^
+ * $
+ * {
+ * }
+ * (
+ * )
+ * |
+ * [
+ * ]
+ * /
+ * \
+ */
 const regexEscapeRE = /[-.*+?^${}()|[\]\/\\]/g
 
+/**
+ * 将自定义的分隔符拼装成正则表达式
+ */
 const buildRegex = cached(delimiters => {
+  // $& 代表为匹配到的子字符串
   const open = delimiters[0].replace(regexEscapeRE, '\\$&')
   const close = delimiters[1].replace(regexEscapeRE, '\\$&')
   return new RegExp(open + '((?:.|\\n)+?)' + close, 'g')
@@ -17,6 +51,9 @@ type TextParseResult = {
   tokens: Array<string | { '@binding': string }>
 }
 
+/**
+ * 处理文本的插值部分
+ */
 export function parseText (
   text: string,
   delimiters?: [string, string]
@@ -33,6 +70,7 @@ export function parseText (
     index = match.index
     // push text token
     if (index > lastIndex) {
+      // 两个插值表达式之间的部分 或 第一个插值表达式的左边部分
       rawTokens.push(tokenValue = text.slice(lastIndex, index))
       tokens.push(JSON.stringify(tokenValue))
     }
@@ -43,6 +81,7 @@ export function parseText (
     lastIndex = index + match[0].length
   }
   if (lastIndex < text.length) {
+    // 文本里不包含插值的最后部分
     rawTokens.push(tokenValue = text.slice(lastIndex))
     tokens.push(JSON.stringify(tokenValue))
   }
